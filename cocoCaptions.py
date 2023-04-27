7cf9a9e500f2e71a6710207ebe25f53b429b1589
erik <epautsch@lu.edu>
2023-04-26 21:24:03 -0500

    training

--- cocoCaptions.py
+++ cocoCaptions.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[ ]:
 
 
-from collections import Counter
+# just checking
 import math
 import os
 import time
@@ -20,14 +20,12 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.optim.lr_scheduler import ReduceLROnPlateau, CosineAnnealingLR, CosineAnnealingWarmRestarts
 import torch.nn.init as init
-import nltk
-nltk.download('punkt')
 import matplotlib.pyplot as plt
-from matplotlib import style
-from transformers import BertTokenizer, BertModel
+from transformers import AutoTokenizer, AutoModel
+from tqdm import tqdm
 
 
-# In[2]:
+# In[ ]:
 
 
 image_transform = Compose([
@@ -38,7 +36,7 @@ image_transform = Compose([
 ])
 
 
-# In[3]:
+# In[ ]:
 
 
 class CaptionPreprocessor:
@@ -60,7 +58,7 @@ class CaptionPreprocessor:
         return [self.preprocess(caption) for caption in captions]
 
 
-# In[4]:
+# In[ ]:
 
 
 class CustomCocoDataset(Dataset):
@@ -79,7 +77,7 @@ class CustomCocoDataset(Dataset):
         return img, preprocessed_caption
 
 
-# In[5]:
+# In[ ]:
 
 
 class PatchEmbedding(nn.Module):
@@ -116,7 +114,7 @@ class VisionTransformer(nn.Module):
         return x
 
 
-# In[6]:
+# In[ ]:
 
 
 class PositionalEncoding(nn.Module):
@@ -129,22 +127,27 @@ class PositionalEncoding(nn.Module):
         self.encoding[:, :, 0::2] = torch.sin(pos * div_term)
         self.encoding[:, :, 1::2] = torch.cos(pos * div_term)
 
+    def forward(self, x):
+        x = x + self.encoding[:, :x.size(1), :]
+        return x
+
 
 class TransformerCaptionDecoder(nn.Module):
-    def __init__(self, bert_model, d_model, num_layers, num_heads, mlp_dim, max_len=128):
+    def __init__(self, auto_model, d_model, num_layers, num_heads, mlp_dim, max_len=128):
         super().__init__()
 
-        self.bert_model = bert_model
+        self.auto_model = auto_model
         self.positional_encoding = PositionalEncoding(d_model, max_len)
         self.transformer_layers = nn.ModuleList([
             nn.TransformerDecoderLayer(d_model, num_heads, mlp_dim)
             for _ in range(num_layers)
         ])
-        self.output_layer = nn.Linear(d_model, self.bert_model.config.vocab_size)
+        self.output_layer = nn.Linear(d_model, self.auto_model.config.vocab_size)
         init.xavier_uniform_(self.output_layer.weight)
 
     def forward(self, captions, memory):
-        captions = self.bert_model.embeddings(captions) + self.positional_encoding.encoding[:, :captions.shape[1]]
+        captions = self.auto_model.embeddings(captions)
+        captions = self.positional_encoding(captions)
 
         for layer in self.transformer_layers:
             captions = layer(captions, memory)
@@ -153,40 +156,37 @@ class TransformerCaptionDecoder(nn.Module):
         return logits
 
 
-# In[7]:
+# In[ ]:
 
 
 class ImageCaptioningModel(nn.Module):
-    def __init__(self, image_encoder, caption_decoder, embedding_size, start_token_index):
+    def __init__(self, image_encoder, caption_decoder):
         super(ImageCaptioningModel, self).__init__()
         self.image_encoder = image_encoder
         self.caption_decoder = caption_decoder
-        self.embedding_size = embedding_size
-        self.start_token_index = start_token_index
+        self.start_token_index = caption_decoder.auto_model.config.bos_token_id or 0
+        self.embedding_size = caption_decoder.auto_model.config.hidden_size
+        self.image_feature_linear = nn.Linear(768, self.embedding_size)
 
     def forward(self, images, captions):
         image_features = self.image_encoder(images)
         num_patches = (224 // 16) * (224 // 16)
-        image_features_flattened = image_features.permute(1, 0, 2).reshape(-1, num_patches, self.embedding_size)
+        # image_features_flattened = image_features.permute(1, 0, 2).reshape(-1, num_patches, self.embedding_size)
 
-        # print("Start token index:", caption_preprocessor.vocab['<start>'])
-
-        start_token_embeddings = self.caption_decoder.embedding(torch.tensor([self.start_token_index], device=images.device)).repeat(image_features.shape[0], 1, 1) # Get the <start> token embedding and repeat it for the batch size
-        image_features_summed = image_features_flattened.sum(dim=1).unsqueeze(1)
+        start_token_tensor = torch.tensor([self.start_token_index], dtype=torch.long, device=images.device)
+        start_token_embeddings = self.caption_decoder.auto_model.embeddings(start_token_tensor).repeat(image_features.shape[0], 1, 1) # getting start token embedding and repeating it for batch size
+        image_features_summed = image_features.sum(dim=1).unsqueeze(1)
+        image_features_summed = self.image_feature_linear(image_features_summed)
         memory = torch.cat([start_token_embeddings, image_features_summed], dim=1) # Concatenate the start token embeddings with the flattened image features
 
         memory = memory.transpose(0, 1)
-
         captions = captions.transpose(0, 1)
 
         output = self.caption_decoder(captions, memory)
-
-        output = output.transpose(0, 1)
-
         return output
 
 
-# In[8]:
+# In[ ]:
 
 
 class NoamScheduler:
@@ -210,7 +210,7 @@ class NoamScheduler:
         return (self.d_model ** -0.5) * min(arg1, arg2)
 
 
-# In[9]:
+# In[ ]:
 
 
 def plot_and_save(train_losses, val_losses, learning_rates, max_min_loss_diffs):
@@ -251,6 +251,9 @@ def plot_and_save(train_losses, val_losses, learning_rates, max_min_loss_diffs):
 # In[ ]:
 
 
+tokenizer_name = 'distilbert-base-uncased'
+tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
+
 train_dataset = CocoCaptions(root='./coco/images',
                        annFile='./coco/annotations/captions_train2014.json',
                        transform=image_transform)
@@ -260,18 +263,19 @@ val_dataset = CocoCaptions(root='./coco/images',
 train_captions = [entry['caption'] for entry in train_dataset.coco.anns.values()]
 val_captions = [entry['caption'] for entry in val_dataset.coco.anns.values()]
 
-max_caption_length_train = max([len(nltk.tokenize.word_tokenize(caption.lower())) for caption in train_captions])
-max_caption_length_val = max([len(nltk.tokenize.word_tokenize(caption.lower())) for caption in val_captions])
+caption_preprocessor = CaptionPreprocessor(train_captions + val_captions, tokenizer)
+
+max_caption_length_train = max([len(tokenized_caption) for tokenized_caption in caption_preprocessor.tokenize_captions(train_captions)])
+max_caption_length_val = max([len(tokenized_caption) for tokenized_caption in caption_preprocessor.tokenize_captions(val_captions)])
 max_caption_length = max(max_caption_length_train, max_caption_length_val)
 print('Maximum caption length (without <start>, <end>, and <pad> tokens):', max_caption_length)
 
-caption_preprocessor = CaptionPreprocessor(train_captions + val_captions)
 custom_train_dataset = CustomCocoDataset(train_dataset, caption_preprocessor, num_captions=5)
 custom_val_dataset = CustomCocoDataset(val_dataset, caption_preprocessor, num_captions=5)
 
 batch_size = 64
-train_data_loader = DataLoader(custom_train_dataset, batch_size=batch_size, shuffle=True, num_workers=4)
-val_data_loader = DataLoader(custom_val_dataset, batch_size=batch_size, shuffle=True, num_workers=4)
+train_data_loader = DataLoader(custom_train_dataset, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True, drop_last=True)
+val_data_loader = DataLoader(custom_val_dataset, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True, drop_last=True)
 
 
 # In[ ]:
@@ -282,19 +286,20 @@ print(device)
 
 image_encoder = VisionTransformer(in_channels=3,
                                   patch_size=16,
-                                  embed_dim=1600,
+                                  embed_dim=768,
                                   num_layers=16,
                                   num_heads=16,
-                                  mlp_dim=5120,
-                                  num_classes=1600).to(device)
-max_caption_index = max([max(caption) for caption in caption_preprocessor.captions_tokenized])
-caption_decoder = TransformerCaptionDecoder(vocab_size=max_caption_index + 1,
-                                            d_model=1600,
-                                            num_layers=20,
-                                            num_heads=20,
-                                            mlp_dim=5120).to(device)
-embedding_size = 1600
-model = ImageCaptioningModel(image_encoder, caption_decoder, embedding_size, caption_preprocessor.vocab['<start>']).to(device)
+                                  mlp_dim=1024,
+                                  num_classes=768).to(device)
+
+auto_model = AutoModel.from_pretrained(tokenizer_name).to(device)
+caption_decoder = TransformerCaptionDecoder(auto_model=auto_model,
+                                            d_model=768,
+                                            num_layers=16,
+                                            num_heads=16,
+                                            mlp_dim=1024).to(device)
+
+model = ImageCaptioningModel(image_encoder, caption_decoder).to(device)
 
 useTwoGPUs = True
 if torch.cuda.device_count() > 1 and useTwoGPUs:
@@ -307,13 +312,13 @@ total_samples = len(train_data_loader.dataset)
 batch_size = train_data_loader.batch_size
 max_iterations = math.ceil(total_samples / batch_size)
 
-criterion = nn.CrossEntropyLoss(ignore_index=caption_preprocessor.vocab['<pad>'])
-optimizer = optim.Adam(model.parameters(), lr=1e-5)
+criterion = nn.CrossEntropyLoss(ignore_index=tokenizer.pad_token_id)
+optimizer = optim.Adam(model.parameters(), lr=1e-3)
 
-# scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=2, verbose=True)
+scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.9, patience=2, verbose=True)
 # scheduler = NoamScheduler(optimizer, d_model=1600, warmup_steps=4000)
 # scheduler = CosineAnnealingLR(optimizer, T_max=num_epochs, eta_min=1e-6)
-scheduler = CosineAnnealingWarmRestarts(optimizer, T_0=int(num_epochs / 5), eta_min=1e-6)
+# scheduler = CosineAnnealingWarmRestarts(optimizer, T_0=int(num_epochs / 5), eta_min=1e-6)
 
 best_val_loss = float('inf')
 
@@ -323,6 +328,48 @@ learning_rates = []
 max_min_loss_diffs = []
 save_name = ''
 
+def train_one_epoch(model, dataloader, criterion, optimizer, device, epoch, avg_every):
+    model.train()
+    train_loss = 0
+    last_x_losses = []
+    for i, (images, captions) in enumerate(tqdm(dataloader, desc='Training')):
+    # for i, (images, captions) in enumerate(dataloader):
+        images = images.to(device)
+        captions_input = captions[:, :-1].to(device)
+        captions_target = captions[:, 1:].to(device)
+
+        optimizer.zero_grad()
+        output = model(images, captions_input)
+
+        loss = criterion(output.reshape(-1, 30522), captions_target.view(-1))
+        loss.backward()
+        optimizer.step()
+
+        train_loss += loss.item()
+        last_x_losses.append(loss.item())
+
+        if i % avg_every == 0:
+            avg_loss = sum(last_x_losses) / len(last_x_losses)
+            print(f'Epoch: {epoch+1}, Iteration: {i}, Loss (last {avg_every} iterations: {avg_loss}')
+    return train_loss / len(dataloader)
+
+def evaluate(model, dataloader, criterion, device):
+    model.eval()
+    val_loss = 0
+    with torch.no_grad():
+        for images, captions in tqdm(dataloader, desc='Validating'):
+        # for images, captions in dataloader:
+            image = images.to(device)
+            captions_input = captions[:, :-1].to(device)
+            captions_target = captions[:, 1:].to(device)
+
+            output = model(images, captions_input)
+            loss = criterion(output.reshape(-1, 30522), captions_target.view(-1))
+
+            val_loss += loss.item()
+    return val_loss / len(dataloader)
+
+
 print('**********STARTING TRAINING**********')
 training_start = time.time()
 for epoch in range(num_epochs):
@@ -331,90 +378,120 @@ for epoch in range(num_epochs):
     epoch_max_loss = float('-inf')
     epoch_min_loss = float('inf')
 
-    model.train()
-    train_loss = 0
-
     print(f'Total samples: {total_samples}, Batch size: {batch_size}, Maximum iterations: {max_iterations}')
 
-    epoch_train_start = time.time()
-    for i, (images, captions) in enumerate(train_data_loader):
-        images = images.to(device)
-        captions_input = captions[:, :-1].to(device)
-        captions_target = captions[:, 1:].to(device)
-
-        optimizer.zero_grad()
-        output = model(images, captions_input)
-
-        loss = criterion(output.reshape(-1, 28796), captions_target.view(-1))
-        loss.backward()
-        optimizer.step()
-
-        train_loss += loss.item()
-
-        if loss.item() > epoch_max_loss:
-            epoch_max_loss = loss.item()
-            print(f'Max loss set to: {epoch_max_loss}')
-        if loss.item() < epoch_min_loss:
-            epoch_min_loss = loss.item()
-            print(f'Min loss set to: {epoch_min_loss}')
-
-        if i % 50 == 0:
-            print(f'Epoch: {epoch+1}/{num_epochs}, Iteration: {i}, Loss: {loss.item()}')
-
-    epoch_train_end = time.time()
-    epoch_train_time = epoch_train_end - epoch_train_start
-    print(f'Epoch {epoch+1} training time: {epoch_train_time}')
-
-    epoch_max_min_diff = epoch_max_loss - epoch_min_loss
-    if epoch + 1 != 1:
-        max_min_loss_diffs.append(epoch_max_min_diff)
-    print(f'Difference between max and min loss in epoch {epoch+1}: {epoch_max_min_diff}')
-
-    train_loss /= len(train_data_loader)
-
-    model.eval()
-    val_loss = 0
-
-    epoch_val_start = time.time()
-    with torch.no_grad():
-        for images, captions in val_data_loader:
-            images = images.to(device)
-            captions_input = captions[:, :-1].to(device)
-            captions_target = captions[:, 1:].to(device)
-
-            output = model(images, captions_input)
-            loss = criterion(output.reshape(-1, 28796), captions_target.view(-1))
-
-            val_loss += loss.item()
-
-    epoch_val_end = time.time()
-    epoch_val_time = epoch_val_end - epoch_val_start
-    print(f'Epoch {epoch+1} validation time: {epoch_val_time}')
+    avg_every = 25
+    train_loss = train_one_epoch(model, train_data_loader, criterion, optimizer, device, epoch, avg_every)
+    val_loss = evaluate(model, val_data_loader, criterion, device)
 
     epoch_end = time.time()
-    epoch_time = epoch_end - epoch_start
-    print(f'Epoch {epoch+1} total time: {epoch_time}')
+    print(f'Epoch {epoch+1} total time: {epoch_end - epoch_start}')
 
-    val_loss /= len(val_data_loader)
-    print(f'Epoch: {epoch+1}/{num_epochs}, Train Loss: {train_loss}, Val Loss: {val_loss}')
     train_losses.append(train_loss)
     val_losses.append(val_loss)
-    learning_rates.append(optimizer.param_groups[0]['lr'])
 
     if val_loss < best_val_loss:
         best_val_loss = val_loss
 
-        if os.path.exists(save_name):
-            os.remove(save_name)
-
-        save_name = f'best_loss_model_{epoch}.pth'
+        save_name = f'best_loss_model_{epoch}.pt'
         torch.save(model.state_dict(), save_name)
 
-    scheduler.step()
+    scheduler.step(val_loss)
 
 training_end = time.time()
-training_time = training_end - training_start
-print(f'Total training time: {training_time}')
+print(f'Total training time: {training_end - training_start}')
 
 plot_and_save(train_losses, val_losses, learning_rates, max_min_loss_diffs)
+
+
+# In[ ]:
+
+
+# epoch_train_start = time.time()
+# for i, (images, captions) in enumerate(train_data_loader):
+#     images = images.to(device)
+#     captions_input = captions[:, :-1].to(device)
+#     captions_target = captions[:, 1:].to(device)
+#
+#     optimizer.zero_grad()
+#     output = model(images, captions_input)
+#
+#     loss = criterion(output.reshape(-1, 28796), captions_target.view(-1))
+#     loss.backward()
+#     optimizer.step()
+#
+#     train_loss += loss.item()
+#
+#     if loss.item() > epoch_max_loss:
+#         epoch_max_loss = loss.item()
+#         print(f'Max loss set to: {epoch_max_loss}')
+#     if loss.item() < epoch_min_loss:
+#         epoch_min_loss = loss.item()
+#         print(f'Min loss set to: {epoch_min_loss}')
+#
+#     if i % 50 == 0:
+#         print(f'Epoch: {epoch+1}/{num_epochs}, Iteration: {i}, Loss: {loss.item()}')
+#
+# epoch_train_end = time.time()
+# epoch_train_time = epoch_train_end - epoch_train_start
+# print(f'Epoch {epoch+1} training time: {epoch_train_time}')
+#
+# epoch_max_min_diff = epoch_max_loss - epoch_min_loss
+# if epoch + 1 != 1:
+#     max_min_loss_diffs.append(epoch_max_min_diff)
+# print(f'Difference between max and min loss in epoch {epoch+1}: {epoch_max_min_diff}')
+#
+# train_loss /= len(train_data_loader)
+#
+# model.eval()
+# val_loss = 0
+
+#     epoch_val_start = time.time()
+#     with torch.no_grad():
+#         for images, captions in val_data_loader:
+#             images = images.to(device)
+#             captions_input = captions[:, :-1].to(device)
+#             captions_target = captions[:, 1:].to(device)
+#
+#             output = model(images, captions_input)
+#             loss = criterion(output.reshape(-1, 28796), captions_target.view(-1))
+#
+#             val_loss += loss.item()
+#
+#     epoch_val_end = time.time()
+#     epoch_val_time = epoch_val_end - epoch_val_start
+#     print(f'Epoch {epoch+1} validation time: {epoch_val_time}')
+#
+#     epoch_end = time.time()
+#     epoch_time = epoch_end - epoch_start
+#     print(f'Epoch {epoch+1} total time: {epoch_time}')
+#
+#     val_loss /= len(val_data_loader)
+#     print(f'Epoch: {epoch+1}/{num_epochs}, Train Loss: {train_loss}, Val Loss: {val_loss}')
+#     train_losses.append(train_loss)
+#     val_losses.append(val_loss)
+#     learning_rates.append(optimizer.param_groups[0]['lr'])
+#
+#     if val_loss < best_val_loss:
+#         best_val_loss = val_loss
+#
+#         if os.path.exists(save_name):
+#             os.remove(save_name)
+#
+#         save_name = f'best_loss_model_{epoch}.pth'
+#         torch.save(model.state_dict(), save_name)
+#
+#     scheduler.step()
+#
+# training_end = time.time()
+# training_time = training_end - training_start
+# print(f'Total training time: {training_time}')
+#
+# plot_and_save(train_losses, val_losses, learning_rates, max_min_loss_diffs)
+
+
+# In[ ]:
+
+
+
 
