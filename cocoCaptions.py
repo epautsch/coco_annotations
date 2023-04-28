60f156dac07fe91c7008e5fcf5d0f2d12c1e5d79
erik <epautsch@lu.edu>
2023-04-27 20:10:57 -0500

    training

--- cocoCaptions.py
+++ cocoCaptions.py
@@ -350,7 +350,7 @@ print('Maximum caption length (without <start>, <end>, and <pad> tokens):', max_
 custom_train_dataset = CustomCocoDataset(train_dataset, caption_preprocessor, num_captions=5)
 custom_val_dataset = CustomCocoDataset(val_dataset, caption_preprocessor, num_captions=5)
 
-batch_size = 64
+batch_size = 128
 train_data_loader = DataLoader(custom_train_dataset, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True, drop_last=True)
 val_data_loader = DataLoader(custom_val_dataset, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True, drop_last=True)
 
