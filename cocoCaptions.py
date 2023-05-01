dd783bf9384f63745bf150e15c992b03612d7989
erik <epautsch@lu.edu>
2023-05-01 07:39:16 -0500

    training

--- cocoCaptions.py
+++ cocoCaptions.py
@@ -171,14 +171,12 @@ class PositionalEncoding(nn.Module):
         encoding = torch.zeros(1, max_len, self.d_model, device=x.device, requires_grad=False)
 
         pos = torch.arange(0, max_len, dtype=torch.float, device=x.device).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0, self.d_model, 2).float() * (-torch.log(torch.tensor(10000.0, device=x.device)) / self.d_model))
+        div_term = torch.exp(torch.arange(0, self.d_model, 2, device=x.device).float() * (-torch.log(torch.tensor(10000.0, device=x.device)) / self.d_model))
         encoding[:, :, 0::2] = torch.sin(pos * div_term)
         encoding[:, :, 1::2] = torch.cos(pos * div_term)
 
         x = x + encoding
         return x
-
-
 
 
 class TransformerCaptionDecoder(nn.Module):
