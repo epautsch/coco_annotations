5f6566d9d9739bb5088506fbd52398c060c84fdd
erik <epautsch@lu.edu>
2023-05-01 06:40:12 -0500

    rest monday

--- cocoCaptions.py
+++ cocoCaptions.py
@@ -8,7 +8,6 @@ import math
 import os
 import time
 import random
-import copy
 import pickle
 
 from torchvision.datasets import CocoCaptions
@@ -25,7 +24,7 @@ from transformers import AutoTokenizer, AutoModel
 from tqdm import tqdm
 
 
-# In[8]:
+# In[2]:
 
 
 # import numpy as np
@@ -44,7 +43,7 @@ from tqdm import tqdm
 # plt.show()
 
 
-# In[ ]:
+# In[3]:
 
 
 image_transform = Compose([
@@ -55,29 +54,50 @@ image_transform = Compose([
 ])
 
 
-# In[ ]:
+# In[80]:
 
 
 class CaptionPreprocessor:
     def __init__(self, captions, tokenizer, max_caption_length=20):
         self.tokenizer = tokenizer
         self.max_caption_length = max_caption_length
+        self.total_caption_length = 0
+        self.total_squared_caption_length = 0
+
+        if not self.tokenizer.bos_token:
+            self.tokenizer.bos_token = '[CLS]'
+        if not self.tokenizer.eos_token:
+            self.tokenizer.eos_token = '[SEP]'
+
         self.captions_tokenized = self.tokenize_captions(captions)
 
     def preprocess(self, caption):
-        tokens = self.tokenizer.tokenize(caption)
+        tokens = [self.tokenizer.bos_token] + self.tokenizer.tokenize(caption) + [self.tokenizer.eos_token]
         caption_indices = self.tokenizer.convert_tokens_to_ids(tokens)
+        caption_length = len(caption_indices)
 
-        if len(caption_indices) < self.max_caption_length:
-            caption_indices += [self.tokenizer.pad_token_id] * (self.max_caption_length - len(caption_indices))
+        if caption_length < self.max_caption_length:
+            caption_indices += [self.tokenizer.pad_token_id] * (self.max_caption_length - caption_length)
+
+        self.total_caption_length += caption_length
+        self.total_squared_caption_length += caption_length**2
 
         return caption_indices[:self.max_caption_length]
 
     def tokenize_captions(self, captions):
         return [self.preprocess(caption) for caption in captions]
 
+    def get_average_caption_length(self):
+        return self.total_caption_length / len(self.captions_tokenized)
 
-# In[ ]:
+    def get_caption_length_standard_deviation(self):
+        mean = self.get_average_caption_length()
+        variance = (self.total_squared_caption_length / len(self.captions_tokenized)) - mean**2
+        std_dev = math.sqrt(max(0, variance))
+        return std_dev
+
+
+# In[81]:
 
 
 class CustomCocoDataset(Dataset):
@@ -91,12 +111,17 @@ class CustomCocoDataset(Dataset):
 
     def __getitem__(self, idx):
         img, caption_list = self.coco_dataset[idx]
+
+        # hits when using test dataset which doesn't have captions
+        if not caption_list:
+            return img, None
+
         selected_caption = random.choice(caption_list[:self.num_captions])
         preprocessed_caption = torch.tensor(self.caption_preprocessor.preprocess(selected_caption))
         return img, preprocessed_caption
 
 
-# In[ ]:
+# In[82]:
 
 
 class PatchEmbedding(nn.Module):
@@ -133,11 +158,11 @@ class VisionTransformer(nn.Module):
         return x
 
 
-# In[ ]:
+# In[83]:
 
 
 class PositionalEncoding(nn.Module):
-    def __init__(self, d_model, max_len=5000):
+    def __init__(self, d_model, max_len=20):
         super().__init__()
         self.encoding = nn.Parameter(torch.zeros(1, max_len, d_model), requires_grad=False)
 
@@ -152,7 +177,7 @@ class PositionalEncoding(nn.Module):
 
 
 class TransformerCaptionDecoder(nn.Module):
-    def __init__(self, auto_model, d_model, num_layers, num_heads, mlp_dim, max_len=128):
+    def __init__(self, auto_model, d_model, num_layers, num_heads, mlp_dim, max_len=20):
         super().__init__()
 
         self.auto_model = auto_model
@@ -175,7 +200,7 @@ class TransformerCaptionDecoder(nn.Module):
         return logits
 
 
-# In[ ]:
+# In[84]:
 
 
 class ImageCaptioningModel(nn.Module):
@@ -204,8 +229,38 @@ class ImageCaptioningModel(nn.Module):
         output = self.caption_decoder(captions, memory)
         return output
 
+    # used for inference with test dataset
+    def sample(self, images, max_len=20):
+        with torch.no_grad():
+            image_features = self.image_encoder(images)
+            num_patches = (224 // 16) * (224 // 16)
 
-# In[1]:
+            start_token_tensor = torch.tensor([self.start_token_index], dtype=torch.long, device=images.device)
+            start_token_embeddings = self.caption_decoder.auto_model.embeddings(start_token_tensor).repeat(image_features.shape[0], 1, 1)
+            image_features_summed = image_features.sum(dim=1).unsqueeze(1)
+            image_features_summed = self.image_feature_linear(image_features_summed)
+            memory = torch.cat([start_token_embeddings, image_features_summed], dim=1)
+            memory = memory.transpose(0, 1)
+
+            input_ids = start_token_tensor.unsqueeze(1).repeat(1, image_features.shape[0]).transpose(0, 1)
+
+            for _ in range(max_len - 1):
+                captions = torch.full((1, 1), self.start_token_index, dtype=torch.long, device=images.device)
+                captions = self.caption_decoder.positional_encoding(captions)
+
+                for layer in self.caption_decoder.transformer_layers:
+                    captions = layer(captions, memory)
+
+                logits = self.caption_decoder.output_layer(captions)
+                next_token_logits = logits[:, -1, :]
+                next_token = torch.argmax(next_token_logits, dim=-1).unsqueeze(1)
+                input_ids = torch.cat([input_ids, next_token], dim=-1)
+
+            return input_ids
+
+
+
+# In[85]:
 
 
 def train_one_epoch(model,
@@ -264,7 +319,7 @@ def evaluate(model, dataloader, criterion, device):
     return val_loss / len(dataloader)
 
 
-# In[ ]:
+# In[86]:
 
 
 class NoamScheduler:
@@ -286,7 +341,7 @@ class NoamScheduler:
         return (self.d_model ** -0.5) * min(arg1, arg2)
 
 
-# In[ ]:
+# In[87]:
 
 
 def plot_and_save(train_losses, val_losses, learning_rates):
@@ -314,7 +369,7 @@ def plot_and_save(train_losses, val_losses, learning_rates):
     fig.savefig('learning_rates.png')
 
 
-# In[ ]:
+# In[88]:
 
 
 def save_lists_to_file(file_path, train_losses, val_losses, learning_rates):
@@ -333,7 +388,19 @@ def load_lists_from_file(file_path):
     return data['train_losses'], data['val_losses'], data['learning_rates']
 
 
-# In[ ]:
+# In[89]:
+
+
+# Needed when running inference on laptop
+def adjust_state_dict_keys(state_dict):
+    new_state_dict = {}
+    for key, value in state_dict.items():
+        new_key = key.replace('module.', '')
+        new_state_dict[new_key] = value
+    return new_state_dict
+
+
+# In[90]:
 
 
 os.environ['TOKENIZERS_PARALLELISM'] = 'true'
@@ -351,20 +418,51 @@ val_captions = [entry['caption'] for entry in val_dataset.coco.anns.values()]
 
 caption_preprocessor = CaptionPreprocessor(train_captions + val_captions, tokenizer)
 
-max_caption_length_train = max([len(tokenized_caption) for tokenized_caption in caption_preprocessor.tokenize_captions(train_captions)])
-max_caption_length_val = max([len(tokenized_caption) for tokenized_caption in caption_preprocessor.tokenize_captions(val_captions)])
+max_caption_length_train = max([len(caption.split()) for caption in train_captions])
+max_caption_length_val = max([len(caption.split()) for caption in val_captions])
 max_caption_length = max(max_caption_length_train, max_caption_length_val)
+
+print(max_caption_length_train, max_caption_length_val, max_caption_length)
 print('Maximum caption length (without <start>, <end>, and <pad> tokens):', max_caption_length)
+
+average_caption_length = caption_preprocessor.get_average_caption_length()
+std_dev_caption_length = caption_preprocessor.get_caption_length_standard_deviation()
+
+print('Average caption length:', average_caption_length)
+print('Standard deviation of caption length:', std_dev_caption_length)
 
 custom_train_dataset = CustomCocoDataset(train_dataset, caption_preprocessor, num_captions=5)
 custom_val_dataset = CustomCocoDataset(val_dataset, caption_preprocessor, num_captions=5)
 
-batch_size = 256
+batch_size = 512
 train_data_loader = DataLoader(custom_train_dataset, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True, drop_last=True)
 val_data_loader = DataLoader(custom_val_dataset, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True, drop_last=True)
 
 
-# In[ ]:
+# In[92]:
+
+
+import numpy as np
+
+def display_random_sample(dataset, tokenizer):
+    random_idx = np.random.randint(0, len(dataset))
+    image, caption = dataset[random_idx]
+    image = image.permute(1, 2, 0).numpy()
+    image = (image * np.array([0.229, 0.224, 0.225])) + np.array([0.485, 0.456, 0.406])
+    image = np.clip(image, 0, 1)
+
+    caption_text = tokenizer.decode(caption.numpy(), skip_special_tokens=False)
+
+    plt.imshow(image)
+    plt.axis('off')
+    plt.title(caption_text)
+    plt.show()
+
+# Display a random sample from the training dataset
+# display_random_sample(custom_train_dataset, tokenizer)
+
+
+# In[73]:
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -373,17 +471,17 @@ print(device)
 image_encoder = VisionTransformer(in_channels=3,
                                   patch_size=16,
                                   embed_dim=768,
-                                  num_layers=8,
-                                  num_heads=12,
-                                  mlp_dim=1024,
+                                  num_layers=4,
+                                  num_heads=8,
+                                  mlp_dim=256,
                                   num_classes=768).to(device)
 
 auto_model = AutoModel.from_pretrained(tokenizer_name).to(device)
 caption_decoder = TransformerCaptionDecoder(auto_model=auto_model,
                                             d_model=768,
-                                            num_layers=8,
-                                            num_heads=12,
-                                            mlp_dim=1024).to(device)
+                                            num_layers=4,
+                                            num_heads=8,
+                                            mlp_dim=256).to(device)
 
 model = ImageCaptioningModel(image_encoder, caption_decoder).to(device)
 
@@ -392,7 +490,7 @@ if torch.cuda.device_count() > 1 and useTwoGPUs:
     print(f'Using {torch.cuda.device_count()} GPUs')
     model = nn.DataParallel(model)
 
-num_epochs = 500
+num_epochs = 100
 
 total_samples = len(train_data_loader.dataset)
 batch_size = train_data_loader.batch_size
@@ -412,13 +510,19 @@ train_losses = []
 val_losses = []
 learning_rates = []
 
-load_best_model = True
-load_final = True
-best_model_path = 'final_model.pt'
-save_lists_path = 'final_data.pkl'
+load_best_model = False
+load_final = False
+best_model_path = 'best_model_monday.pt'
+save_lists_path = 'best_data_monday.pkl'
 if load_best_model and os.path.exists(best_model_path):
-    checkpoint = torch.load(best_model_path)
-    model.load_state_dict(checkpoint['model_state_dict'])
+    if torch.cuda.is_available():
+        checkpoint = torch.load(best_model_path)
+        model.load_state_dict(checkpoint['model_state_dict'])
+    else:
+        checkpoint = torch.load(best_model_path, map_location=torch.device('cpu'))
+        adjusted_state_dict = adjust_state_dict_keys(checkpoint['model_state_dict'])
+        model.load_state_dict(adjusted_state_dict)
+
     optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
     scheduler.__dict__.update(checkpoint['scheduler_state_dict'])
     best_val_loss = checkpoint['best_val_loss']
@@ -444,7 +548,7 @@ for epoch in training_range:
 
     print(f'Total samples: {total_samples}, Batch size: {batch_size}, Maximum iterations: {max_iterations}')
 
-    avg_every = 50
+    avg_every = 20
     old_lr = optimizer.param_groups[0]['lr']
 
     train_loss = train_one_epoch(model, train_data_loader, criterion, optimizer, scheduler, device, epoch, num_epochs, avg_every, learning_rates)
@@ -456,6 +560,7 @@ for epoch in training_range:
 
     val_loss = evaluate(model, val_data_loader, criterion, device)
     print(f'VALIDATION LOSS FOR EPOCH {epoch + 1}: {val_loss:.4f}')
+    print(f'CURRENT BEST VALIDATION LOSS: {best_val_loss:.4f}')
 
     epoch_end = time.time()
     print(f'Epoch {epoch+1}/{num_epochs} total time: {epoch_end - epoch_start}')
@@ -483,8 +588,8 @@ for epoch in training_range:
 
     if epoch == num_epochs - 1:
         final_val_loss = val_loss
-        final_save_name = 'final_model.pt'
-        final_save_lists = 'final_data.pkl'
+        final_save_name = 'final_model_monday.pt'
+        final_save_lists = 'final_data_monday.pkl'
 
         torch.save({
             'model_state_dict': model.state_dict(),
@@ -505,5 +610,32 @@ plot_and_save(train_losses, val_losses, learning_rates)
 # In[ ]:
 
 
+# tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
+#
+# test_dataset = CocoCaptions(root='./coco/images',
+#                             annFile='./coco/annotations/image_info_test2014.json',
+#                             transform=image_transform)
+# custom_test_dataset = CustomCocoDataset(test_dataset, caption_preprocessor, num_captions=5)
 
+
+# In[ ]:
+
+
+# def generate_caption(model, image, tokenizer, device, max_len=20):
+#     model.eval()
+#     with torch.no_grad():
+#         image = image.unsqueeze(0).to(device)
+#         generated_caption = model.sample(image, max_len)
+#         decoded_caption = tokenizer.decode(generated_caption.squeeze().tolist(), skip_special_tokens=True)
+#     return decoded_caption
+
+
+# In[ ]:
+
+
+# random_idx = random.randint(0, len(custom_test_dataset) - 1)
+# image, _ = custom_test_dataset[random_idx]
+#
+# generated_caption = generate_caption(model, image, tokenizer, device)
+# print('Generated caption:', generated_caption)
 
