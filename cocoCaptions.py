49a2ab57897933d2bb8bbd6d80f2f26290b7f34f
erik <epautsch@lu.edu>
2023-05-04 22:33:50 -0500

    noam warmup set to 20000, added weight decay

--- cocoCaptions.py
+++ cocoCaptions.py
@@ -532,10 +532,10 @@ batch_size = train_data_loader.batch_size
 max_iterations = math.ceil(total_samples / batch_size)
 
 criterion = nn.CrossEntropyLoss(ignore_index=tokenizer.pad_token_id)
-optimizer = optim.Adam(model.parameters(), lr=0)  #, weight_decay=1e-5)
+optimizer = optim.Adam(model.parameters(), lr=0, weight_decay=1e-5)
 
 # scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.67, patience=2, verbose=True)
-scheduler = NoamScheduler(optimizer, d_model=768, warmup_steps=4000)
+scheduler = NoamScheduler(optimizer, d_model=768, warmup_steps=20000)
 # scheduler = CosineAnnealingLR(optimizer, T_max=num_epochs, eta_min=1e-6)
 # scheduler = CosineAnnealingWarmRestarts(optimizer, T_0=int(num_epochs / 5), eta_min=1e-6)
 
