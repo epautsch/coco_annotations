a6a68077848a2c7933dd2af8233fc3ff85617277
erik <epautsch@lu.edu>
2023-05-05 13:44:29 -0500

    training

--- cocoCaptions.py
+++ cocoCaptions.py
@@ -302,9 +302,8 @@ def train_one_epoch(model,
 
         if i % avg_every == 0 and i != 0:
             avg_loss = sum(last_x_losses) / len(last_x_losses)
-
-            progress_bar.set_postfix(avg_loss_last_x_iterations=f'last {avg_every}: {avg_loss:.4f}')
-
+            print(f'Epoch: {epoch+1}/{num_epochs}, Iteration: {i}, Loss (last {avg_every} iterations): {avg_loss:.4f}')
+            # progress_bar.set_postfix(avg_loss_last_x_iterations=f'last {avg_every}: {avg_loss:.4f}')
             last_x_losses = []
 
     return train_loss / len(dataloader)
