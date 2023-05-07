3936ec582016c7bae7487812a00197285123e78b
erik <epautsch@lu.edu>
2023-05-07 16:56:08 -0500

    training

--- cocoCaptions.py
+++ cocoCaptions.py
@@ -552,7 +552,7 @@ if torch.cuda.device_count() > 1 and useTwoGPUs:
     print(f'Using {torch.cuda.device_count()} GPUs')
     model = nn.DataParallel(model)
 
-num_epochs = 80
+num_epochs = 100
 
 total_samples = len(train_data_loader.dataset)
 batch_size = train_data_loader.batch_size
@@ -579,8 +579,8 @@ learning_rates = []
 
 load_best_model = True
 load_final = True
-best_model_path = 'larger_attempt_3_FINAL_41_60.pt'
-save_lists_path = 'larger_attempt_3_FINAL_41_60.pkl'
+best_model_path = 'larger_attempt_3_FINAL_61_80.pt'
+save_lists_path = 'larger_attempt_3_FINAL_61_80.pkl'
 if load_best_model and os.path.exists(best_model_path):
     if torch.cuda.is_available():
         checkpoint = torch.load(best_model_path)
@@ -675,8 +675,8 @@ for epoch in training_range:
 
     if epoch == num_epochs - 1:
         final_val_loss = best_val_loss
-        final_save_name = 'larger_attempt_3_FINAL_61_80.pt'
-        final_save_lists = 'larger_attempt_3_FINAL_61_80.pkl'
+        final_save_name = 'larger_attempt_3_FINAL_81_100.pt'
+        final_save_lists = 'larger_attempt_3_FINAL_81_100.pkl'
 
         torch.save({
             'model_state_dict': model.state_dict(),
