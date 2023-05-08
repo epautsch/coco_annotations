54b50c2635528883b1be15cb2bb5a7884c520e3d
erik <epautsch@lu.edu>
2023-05-08 00:42:24 -0500

    training with tf

--- cocoCaptions.py
+++ cocoCaptions.py
@@ -551,7 +551,7 @@ if torch.cuda.device_count() > 1 and useTwoGPUs:
     print(f'Using {torch.cuda.device_count()} GPUs')
     model = nn.DataParallel(model)
 # start newwwwwwww
-num_epochs = 140
+num_epochs = 160
 
 total_samples = len(train_data_loader.dataset)
 batch_size = train_data_loader.batch_size
@@ -578,8 +578,8 @@ learning_rates = []
 
 load_best_model = True
 load_final = True
-best_model_path = 'larger_attempt_3_FINAL_101_120_tf_0_9.pt'
-save_lists_path = 'larger_attempt_3_FINAL_101_120_tf_0_9.pkl'
+best_model_path = 'larger_attempt_3_FINAL_121_140_tf_0_8.pt'
+save_lists_path = 'larger_attempt_3_FINAL_121_140_tf_0_8.pkl'
 if load_best_model and os.path.exists(best_model_path):
     if torch.cuda.is_available():
         checkpoint = torch.load(best_model_path)
@@ -673,12 +673,12 @@ for epoch in training_range:
             # 'tf_scheduler_state_dict': tf_scheduler.__dict__
         }, save_name)
         save_lists_to_file(save_lists_path, train_losses, val_losses, learning_rates)
-        print(f'**********NEW BEST MODEL SAVED @ VAL: {best_val_loss:.4f}**********')
+        print(f'********************NEW BEST MODEL SAVED @ VAL: {best_val_loss:.4f}********************')
 
     if epoch == num_epochs - 1:
         final_val_loss = best_val_loss
-        final_save_name = 'larger_attempt_3_FINAL_121_140_tf_0_8.pt'
-        final_save_lists = 'larger_attempt_3_FINAL_121_140_tf_0_8.pkl'
+        final_save_name = 'larger_attempt_3_FINAL_141_160_tf_0_8.pt'
+        final_save_lists = 'larger_attempt_3_FINAL_141_160_tf_0_8.pkl'
 
         torch.save({
             'model_state_dict': model.state_dict(),
