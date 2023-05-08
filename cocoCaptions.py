f5a1f71b04bde09d3b32e09356cf1722b7cfbe2c
erik <epautsch@lu.edu>
2023-05-08 06:26:56 -0500

    training with tf

--- cocoCaptions.py
+++ cocoCaptions.py
@@ -551,7 +551,7 @@ if torch.cuda.device_count() > 1 and useTwoGPUs:
     print(f'Using {torch.cuda.device_count()} GPUs')
     model = nn.DataParallel(model)
 # start newwwwwwww
-num_epochs = 180
+num_epochs = 220
 
 total_samples = len(train_data_loader.dataset)
 batch_size = train_data_loader.batch_size
@@ -578,8 +578,8 @@ learning_rates = []
 
 load_best_model = True
 load_final = True
-best_model_path = 'larger_attempt_3_FINAL_161_170_tf_0_9.pt'
-save_lists_path = 'larger_attempt_3_FINAL_161_170_tf_0_9.pkl'
+best_model_path = 'larger_attempt_3_FINAL_171_180_tf_0_7.pt'
+save_lists_path = 'larger_attempt_3_FINAL_171_180_tf_0_7.pkl'
 if load_best_model and os.path.exists(best_model_path):
     if torch.cuda.is_available():
         checkpoint = torch.load(best_model_path)
@@ -678,8 +678,8 @@ for epoch in training_range:
 
     if epoch == num_epochs - 1:
         final_val_loss = best_val_loss
-        final_save_name = 'larger_attempt_3_FINAL_171_180_tf_0_7.pt'
-        final_save_lists = 'larger_attempt_3_FINAL_171_180_tf_0_7.pkl'
+        final_save_name = 'larger_attempt_3_FINAL_181_220_tf_0_7.pt'
+        final_save_lists = 'larger_attempt_3_FINAL_181_220_tf_0_7.pkl'
 
         torch.save({
             'model_state_dict': model.state_dict(),
